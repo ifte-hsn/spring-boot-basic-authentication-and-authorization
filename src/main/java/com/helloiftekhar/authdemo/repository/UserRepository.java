557a8243d06555303258df82a3ba780f4b13582a
iftekhar_hossain <iftekhar.hossain@sonalibank.com.bd>
2023-06-15 11:38:40 +0600

    added findByResetPasswordToken for finding user by password token

--- src/main/java/com/helloiftekhar/authdemo/repository/UserRepository.java
+++ src/main/java/com/helloiftekhar/authdemo/repository/UserRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     public User findByEmail(String email);
+
+    public User findByResetPasswordToken(String token);
 }
