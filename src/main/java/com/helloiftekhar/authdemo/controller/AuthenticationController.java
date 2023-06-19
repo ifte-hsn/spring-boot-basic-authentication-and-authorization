200f40b585ada84fae5b0f0b7a0f1b1ebe872b0b
iftekhar_hossain <iftekhar.hossain@sonalibank.com.bd>
2023-06-19 13:42:01 +0600

    prevent user to go back to login page after logged in

--- src/main/java/com/helloiftekhar/authdemo/controller/AuthenticationController.java
+++ src/main/java/com/helloiftekhar/authdemo/controller/AuthenticationController.java
@@ -5,6 +5,9 @@ import com.helloiftekhar.authdemo.model.User;
 import com.helloiftekhar.authdemo.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -24,7 +27,12 @@ public class AuthenticationController {
 
     @GetMapping("/login")
     public String login() {
-        return "login";
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+            return "login";
+        }
+        return "redirect:/";
     }
 
 
